3e23eb9a9a27302df32e64d67e2fa9a590b6c114
NTParker <noahtmparker@gmail.com>
2022-06-15 21:35:00 -0400

    initial function setup

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,17 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // Prompt user for password criteria
+  //  length 8 < 128, lowercase, uppercase, numbers, special characters
+  // Validate input
+  // Generate Password
+  // Display password on the page
+
+  return "Generated Password";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +19,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
