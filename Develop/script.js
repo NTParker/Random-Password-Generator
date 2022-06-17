976cfb980074a5017ffdf932b933a23dcdc1d95f
NTParker <noahtmparker@gmail.com>
2022-06-17 17:09:18 -0400

    added character arrays and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,136 @@
-// Assignment code here
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+var lowerCaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Generate Password
 function generatePassword() {
-  // Prompt user for password criteria
-  //  length 8 < 128, lowercase, uppercase, numbers, special characters
-  // Validate input
-  // Generate Password
-  // Display password on the page
-
   return "Generated Password";
+}
+
+// Prompt user for password criteria
+function getPrompts() {
+  choiceArr = [];
+  characterLength = parseInt(
+    prompt(
+      "Please indicate the desired character length for your password (8 - 128 characters)"
+    )
+  );
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please enter a valid response (A number between 8 - 128)");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  return true;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var correctPrompts = getPrompts();
 
-  passwordText.value = password;
+  if (correctPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
